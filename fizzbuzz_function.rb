81709b2152047140826b8a78fdfe672c230a5aa0
Aaron Gooden <aagooden@me.com>
2018-03-08 11:32:32 -0500

    tested index in array

--- fizzbuzz_function.rb
+++ fizzbuzz_function.rb
@@ -1,4 +1,8 @@
 def array_1_to_100()
 	array = []
+	100.times do|x|
+		array << (x + 1)
+	end
+	p array
 	return array
 end
--- fizzbuzz_test.rb
+++ fizzbuzz_test.rb
@@ -14,7 +14,27 @@ class Tdd_fizzbuzz_test < Minitest::Test
 
 	def test_array
 		new_array = array_1_to_100()
-		assert_equal(Array,new_array)
+		assert_equal(Array,new_array.class)
+	end
+
+	def test_array_1_to_100
+		assert_equal(100,array_1_to_100().length)
+	end
+
+	# def test_array_1_through_100
+	# 	assert_equal()
+	# end
+
+	def test_1_is_1
+		assert_equal(1,array_1_to_100()[0])
+	end
+
+	def test_5_is_5
+		assert_equal(5,array_1_to_100[4])
+	end
+
+	def test_100_is_100
+		assert_equal(100,array_1_to_100[99])
 	end
 
 end
